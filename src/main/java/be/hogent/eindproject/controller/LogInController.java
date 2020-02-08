d47b0a0737546523ebab5f2296cb1e648a83378f
Andy Landtsheer <andy.landtsheer@pandora.be>
2020-02-08 17:06:08 +0100

    commit of the day    :-)
    refactored so that cafe doesn't have any components

--- src/main/java/be/hogent/eindproject/controller/LogInController.java
+++ src/main/java/be/hogent/eindproject/controller/LogInController.java
@@ -1,17 +1,50 @@
 package be.hogent.eindproject.controller;
 
+import be.hogent.eindproject.controller.DTO.WaiterDTO;
+import be.hogent.eindproject.controller.DTO.mappers.WaiterMapper;
 import be.hogent.eindproject.model.infrastructure.WaiterRepository;
 import be.hogent.eindproject.model.model.Waiter;
 
 public class LogInController {
     private WaiterRepository waiterRepository;
+    private WaiterDTO loggedInWaiterDTO;
+    private boolean loggedIn = false;
 
     public LogInController() {
         this.waiterRepository = new WaiterRepository();
+        setNoBodyLoggedInWaiterDTO();
+    }
+
+    //for testing with Mocked waiterRepository
+    LogInController(WaiterRepository waiterRepository) {
+        this.waiterRepository = waiterRepository;
+        setNoBodyLoggedInWaiterDTO();
     }
 
     public boolean checkLogin(int waiterId, String password) {
         Waiter waiter = waiterRepository.findByID(waiterId);
-        return waiter.getPassword().equals(password);
+        if (waiter.getPassword().equals(password)) {
+            loggedInWaiterDTO = WaiterMapper
+                    .mapToWaiterDTO(
+                            waiterRepository.findByID(waiterId));
+            loggedIn = true;
+        }
+        return loggedIn;
+    }
+
+    public WaiterDTO getLoggedInWaiterDTO() {
+        return loggedInWaiterDTO;
+    }
+
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
+
+    public void logOut() {
+        loggedIn = false;
+    }
+
+    private void setNoBodyLoggedInWaiterDTO() {
+        loggedInWaiterDTO = null;
     }
 }
---
+++ src/main/java/be/hogent/eindproject/view/components/TableGrid.java
@@ -0,0 +1,11 @@
+package be.hogent.eindproject.view.components;
+
+import javafx.scene.layout.GridPane;
+
+public class TableGrid {
+    private GridPane tableGrid = new GridPane();
+
+    public GridPane getTableGrid() {
+        return tableGrid;
+    }
+}
